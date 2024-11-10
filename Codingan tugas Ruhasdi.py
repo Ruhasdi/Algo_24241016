e109eb89e945b65db119401f79f49fb56b19a4da
Ruhasdi <ruhasdimusa2004@gmail.com>
2024-11-10 20:29:41 +0700

    Delete Codingan tugas Ruhasdi.py

--- Codingan tugas Ruhasdi.py
+++
@@ -1,7 +0,0 @@
-# input dari user
-data = input ("masukan data")
-print("data : ", data ,"tipe : ", type(data))
-
-#jika input adalah angka  maka casting
-angka = float ("input masukan angka : ", type(data))
-print("data : ", angka, "tipe : ", type(angka))
