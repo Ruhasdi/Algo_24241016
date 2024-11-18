afabafe6be595afbd505688d0bb61c61ff38a037
Ruhasdi <ruhasdimusa2004@gmail.com>
2024-11-18 21:33:40 +0700

    Delete MODUL 3 directory

--- MODUL 3/04-InputUser/main.py
+++
@@ -1,8 +0,0 @@
-# input dari user
-data = input("masukkan data : ")
-print("data : ", data, "Tipe : ", type(data))
-
-# jika adalah adalah angka maka casting 
-angka = float(input( "masukkan angka : "))
-print
-      
