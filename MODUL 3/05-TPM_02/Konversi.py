db846fbfb5054903719a979112ba257b933c5d74
Ruhasdi <ruhasdimusa2004@gmail.com>
2024-11-18 21:26:05 +0800

    Delete MODUL 3/05-TPM_02/Konversi.py

--- MODUL 3/05-TPM_02/Konversi.py
+++
@@ -1,8 +0,0 @@
-# Menetapkan suhu dalam Fahrenheit
-fahrenheit = 130
-
-# Mengonversi fahrenheit ke celsius
-celsius = (fahrenheit - 32) * 5/9
-
-# Menampilkan hasil konversi
-print(" Suhu dalam Celsius adalah:", round(celsius, 2))
