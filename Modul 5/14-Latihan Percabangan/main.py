824baef7ebca80a640f787149e0f185255e66fb0
Ruhasdi <ruhasdimusa2004@gmail.com>
2024-12-09 16:03:52 +0800

    Add files via upload

---
+++ Modul 5/14-Latihan Percabangan/main.py
@@ -0,0 +1,9 @@
+# latihan
+total_belanja = int("input")("masukan total Rp.")
+
+if total > 50000:
+    print("anda mendapatkan mouse dan keyboard")
+else:
+    print("belanja anda kurang dari RP. 500.000")
+    print("anda hanya dapat gantungan kunci")
+print("terima kasih")
